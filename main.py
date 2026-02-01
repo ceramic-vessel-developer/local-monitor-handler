082d828fb39a19611d8b936a468b3a1faf00c175
Kacper Góral <kacper.goral1202@gmail.com>
2026-02-02 00:13:30 +0100

    add return values mocks

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from axela_devtools.monitoring import MonitoringHandler
 
 
 class MonitoringHandlerImpl(MonitoringHandler):
-    def check_camera_status(self):
-        pass
-    def check_darkness_level(self):
-        pass
-    def check_outdoor(self):
-        pass
+    def check_camera_status(self) -> str:
+        return "The camera is online and functioning normally."
 
+    def check_darkness_level(self) -> str:
+        return "The ambient light level is moderate. No low-light conditions detected."
+
+    def check_outdoor(self) -> str:
+        return "There is no one outdoor"
